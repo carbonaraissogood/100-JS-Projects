5222fa87e3d952cdf14efc0589f655d9a06c4637
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-04-21 16:26:07 +0800

    Add tip calculator functionality

---
+++ scripts/2-tip-calculator.js
@@ -0,0 +1,23 @@
+let totalAmount = 0;
+
+function calculateTotalAmount(billAmount, tipPercentage) {
+  console.log('im here');
+  console.log(billAmount);
+  console.log(tipPercentage);
+  return totalAmount = billAmount * (1 + tipPercentage / 100);
+}
+
+document.querySelector('.js-calculate-button').addEventListener('click', () => {
+  const billAmount = Number(document.querySelector('.js-bill-amount').value);
+
+  const tipPercentage = Number(document.querySelector('.js-tip-percentage').value);
+
+  totalAmount = calculateTotalAmount(billAmount, tipPercentage);
+
+  const totalPaymentContainer = document.querySelector('.js-total-payment');
+
+  totalPaymentContainer.innerHTML += `${(totalAmount).toFixed(2)}`;
+
+  totalPaymentContainer.classList.add('bold');
+
+});
