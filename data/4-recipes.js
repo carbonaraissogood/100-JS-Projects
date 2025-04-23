f85d4b5140f3c3b6ce93624a6f082d281d80e963
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-04-23 16:54:41 +0800

    Add initial recipes data structure with Yuzu Jelly and Chicken Parmesan

---
+++ data/4-recipes.js
@@ -0,0 +1,14 @@
+export const recipes = [
+  {
+    image: 'https://www.foodista.com/sites/default/files/yuzu%20jelly%20054.jpg',
+    name: 'Yuzu Jelly',
+    ingredients: '1 litre Yuzu juice drink 100ml Water 1 packet Konnyaku powder extra yuzu juice drink',
+    link: 'https://www.foodista.com/recipe/8LJTCDSH/yuzu-jelly'
+  }, {
+    image: 'http://cloud.foodista.com/content/images/2261ed7e65082c939ae51ed77fd90493ce330ab9_607x400.jpg',
+    name: 'Chicken Parmesan With Pasta',
+    ingredients:
+      '2 breasts of chicken, cut in half, into two fillets Salt and Pepper Flour for dredging 1 Egg Breadcrumbs for coating Garlic powder 2 tablespoons vegetable oil Angel Hair pasta Red pasta sauce Shredded mozzarella cheese',
+    link: 'https://www.foodista.com/recipe/SGWW2KYZ/chicken-parmesan-with-pasta'
+  }
+];
